a8b2e9025fb79f6ed9c93a22a70c0a933db16f6f
apijohn <apijohn0434@cchntek.com>
2019-09-10 15:20:24 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/user", userHandler)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		log.Println(<-sigint)
+		// We received an interrupt signal, shut down.
+		if err := srv.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	log.Print("Server Started")
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
+	log.Print("Server Stopped")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World."))
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(10 * time.Second)
+	w.Write([]byte("Hello user"))
+}
+
